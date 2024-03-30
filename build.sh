b477f2e40b0afe63cca50c48030af2034465bff7
Creeeeger <bauaasgd@gmail.com>
2024-03-30 21:07:57 +0900

    change to beyond2lte idk what s10 device it is

--- build.sh
+++ build.sh
@@ -53,13 +53,22 @@ d2x)
 esac
 '
 
+
+'
 #Select your model here (I set it to beyondx since i have one) so demand change it to yours
 MODEL=beyondx
 export MODEL=beyondx
 KERNEL_DEFCONFIG=exynos9820-beyondx_defconfig
 SOC=9820
 BOARD=SRPSC04B011KU
-    
+'
+MODEL=beyond2lte
+export MODEL=beyond2lte
+KERNEL_DEFCONFIG=exynos9820-beyond2lte_defconfig
+SOC=9820
+BOARD=SRPRI17C014KU
+
+
 FUNC_BUILD_KERNEL()
 {
     echo " Starting a kernel build using "$KERNEL_DEFCONFIG ""
