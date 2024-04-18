346512bb438e22a6bf5fe98a59efeda375801786
Creeeeger <bauaasgd@gmail.com>
2024-04-18 15:40:25 +0100

    Build: add required tools

--- build.sh
+++ build.sh
@@ -1,73 +1,33 @@
 #!/bin/bash
-
-export MODEL=$1
-export BUILD_CROSS_COMPILE=$(pwd)/toolchains/aarch64-linux-android-4.9/bin/aarch64-linux-androidkernel-
-export BUILD_JOB_NUMBER=`grep -c ^processor /proc/cpuinfo`
+export BUILD_CROSS_COMPILE=
+export BUILD_JOB_NUMBER=$(grep -c ^processor /proc/cpuinfo)
 RDIR=$(pwd)
 
-case $MODEL in
-beyond2lte)
-    KERNEL_DEFCONFIG=exynos9820-beyond2lte_defconfig
-    SOC=9820
-    BOARD=SRPRI17C014KU
-;;
-beyond1lte)
-    KERNEL_DEFCONFIG=exynos9820-beyond1lte_defconfig
-    SOC=9820
-    BOARD=SRPRI28B014KU
-;;
-beyond0lte)
-    KERNEL_DEFCONFIG=exynos9820-beyond0lte_defconfig
-    SOC=9820
-    BOARD=SRPRI28A014KU
-;;
-beyondx)
-    KERNEL_DEFCONFIG=exynos9820-beyondx_defconfig
-    SOC=9820
-    BOARD=SRPSC04B011KU
-;;
-d1)
-    KERNEL_DEFCONFIG=exynos9820-d1_defconfig
-    SOC=9825
-    BOARD=SRPSD26B007KU
-;;
-d1x)
-    KERNEL_DEFCONFIG=exynos9820-d1xks_defconfig
-    SOC=9825
-    BOARD=SRPSD23A002KU
-;;
-d2s)
-    KERNEL_DEFCONFIG=exynos9820-d2s_defconfig
-    SOC=9825
-    BOARD=SRPSC14B007KU
-;;
-d2x)
-    KERNEL_DEFCONFIG=exynos9820-d2x_defconfig
-    SOC=9825
-    BOARD=SRPSC14C007KU
-;;
-*)
-    echo "Unknown device: $MODEL setting to beyond2lte"
-    KERNEL_DEFCONFIG=exynos9820-beyond2lte_defconfig
-    SOC=9820
-    BOARD=SRPRI17C014KU
-esac
+'
+MODEL=d2s
+export MODEL=d2s
+KERNEL_DEFCONFIG=exynos9820-d2s_defconfig
+SOC=9825
+BOARD=SRPSC14B007KU
+'
+
+MODEL=beyondx
+export MODEL=beyondx
+KERNEL_DEFCONFIG=exynos9820-beyondx_defconfig
+SOC=9820
+BOARD=SRPSC04B011KU
 
 FUNC_BUILD_KERNEL()
 {
     echo " Starting a kernel build using "$KERNEL_DEFCONFIG ""
-    # No this is not a typo, samsung left it this way on 12
     export PLATFORM_VERSION=11
     export ANDROID_MAJOR_VERSION=r
+        
+    make ARCH=arm64 CC=clang CROSS_COMPILE=$BUILD_CROSS_COMPILE -j$BUILD_JOB_NUMBER $KERNEL_DEFCONFIG
+    
+    make ARCH=arm64 CC=clang CROSS_COMPILE=$BUILD_CROSS_COMPILE -j$BUILD_JOB_NUMBER
 
-    make -j$BUILD_JOB_NUMBER ARCH=arm64 \
-        CROSS_COMPILE=$BUILD_CROSS_COMPILE \
-        $KERNEL_DEFCONFIG || exit -1
-
-    make -j$BUILD_JOB_NUMBER ARCH=arm64 \
-        CROSS_COMPILE=$BUILD_CROSS_COMPILE || exit -1
-
-    $RDIR/toolchains/mkdtimg cfg_create build/dtb_$SOC.img \
+    mkdtboimg cfg_create build/dtb_$SOC.img \
         $RDIR/toolchains/configs/exynos$SOC.cfg \
         -d $RDIR/arch/arm64/boot/dts/exynos
 
@@ -76,7 +36,7 @@ FUNC_BUILD_KERNEL()
 
 FUNC_BUILD_DTBO()
 {
-    $RDIR/toolchains/mkdtimg cfg_create build/dtbo_$MODEL.img \
+    mkdtboimg cfg_create build/dtbo_$MODEL.img \
         $RDIR/toolchains/configs/$MODEL.cfg \
         -d $RDIR/arch/arm64/boot/dts/samsung
 }
